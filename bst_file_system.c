2c3c6c8a9d409a459359b64f569c8340310f3d7a
Kiransugur <01fe24bec256@kletech.ac.in>
2026-04-21 11:10:32 +0530

    Added BST file system code

---
+++ bst_file_system.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Inorder traversal
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    // Example file sizes
+    int files[] = {50, 30, 70, 20, 40, 60, 80};
+    int n = sizeof(files)/sizeof(files[0]);
+
+    for(int i = 0; i < n; i++) {
+        root = insert(root, files[i]);
+    }
+
+    printf("Sorted File Sizes: ");
+    inorder(root);
+
+    return 0;
+}
